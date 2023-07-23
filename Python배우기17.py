7f5b063626cea75c0484260054886ef4800e8a30
문태진 <mtj1015@naver.com>
2023-07-23 23:49:44 +0900

    v2

--- Python배우기17.py
+++ Python배우기17.py
@@ -1,19 +1,11 @@
 # 세 수
-arrayMax_second = input().split()
-
-currentMax, secondMax = arrayMax_second[0], arrayMax_second[0]
-for i in range(len(arrayMax_second)):
-    if currentMax <= arrayMax_second[i]: # 현재 최댓값보다 더 큰 값이 존재하는 경우
-        secondMax = currentMax # 현재의 최댓값을 두번째 최댓값으로 넘겨줌
-        currentMax = arrayMax_second[i] # 새로운 최대값을 현의 최ㅅ값으로 가져옴
-
-    else: # 현재 최대값보다 더 작은 값이 나타난 경우 | 
-        if secondMax < arrayMax_second[i]: # 현재 최대값보다는 작지만 두번째 최대값보다는 큰 경우
-            secondMax = arrayMax_second[i]
-        else: 
-            if secondMax == currentMax and i < 2:
-                secondMax = arrayMax_second[i]
-
-print(currentMax, secondMax) 
-        
-
+# 입력 받아오기
+array = input().split()
+# 문자열 리스트 -> 정수 리스트
+for i in range(len(array)):
+    array[i] = int(array[i])
+# 최댓값을 찾기
+currentMax = max(array)
+# 리스트에서 최대값 제거
+array.remove(currentMax)
+print(max(array))
